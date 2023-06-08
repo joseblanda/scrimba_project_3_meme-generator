ee0f1c175050d2b7bfd8d0b84a6b08e1c71bcafb
Jose Landa <joseblanda@gmail.com>
2023-06-08 11:38:22 -0500

    Added button functionality

--- src/Meme.js
+++ src/Meme.js
@@ -1,10 +1,14 @@
+import React from "react"
 import './App.css'
 import memesData from './memesData.js'
 
 export default function Meme () {
     
+    const [memeImage, setMemeImage] = React.useState("")
+
     function getMeme () {
-        console.log(memesData.data.memes[Math.floor(Math.random()*100)].url)
+        // console.log(memesData.data.memes[Math.floor(Math.random()*100)].url)
+        setMemeImage(memesData.data.memes[Math.floor(Math.random()*100)].url)
     }
 
     return (
@@ -14,7 +18,7 @@ export default function Meme () {
                 <input className='App-form-input' type='text' placeholder='Bottom Text'></input>
                 <button className='App-form-button' onClick={getMeme}>Get a new meme image 🖼</button>
             </div>
-            <img className='App-form-img' src={memesData.data.memes[0].url}/>
+            <img className='App-form-img' src={memeImage}/>
         </main>
     )
 }
